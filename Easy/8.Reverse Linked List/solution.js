d011d4ceabc3624694ed3ad3cab7bcd03fce07fb
alaosama <alahyaneosama1@gmail.com>
2024-09-22 09:00:46 +0200

    LeetCode Playlist

---
+++ Easy/8.Reverse Linked List/solution.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let pre = null;
+    while (head) {
+        [head.next, pre, head] = [pre, head, head.next];
+    }
+    return pre;
+};
