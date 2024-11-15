fd8a12021655cc86492be11683bd47299dda3229
alaosama <alahyaneosama1@gmail.com>
2024-11-15 20:33:50 +0100

    Leetcode Playlist

---
+++ Easy/48.Palindrome Linked List/solution.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let values = [];
+    while (head) {
+        values.push(head.val);
+        head = head.next;
+    }
+    return values.join('') === values.reverse().join('');
+};
