be8391cee3d9335bd5255c30a0e3ac9688e244af
aos01 <osama.alahyane@ecoles-epsi.net>
2025-04-23 12:07:26 +0200

    LeetCode Playlist

---
+++ Medium/1.Zigzag Conversion/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1 || s.length <= numRows) return s;
+    const rows = Array.from({ length: Math.min(numRows, s.length)}, () => "");
+    let currRow = 0;
+    let goingDown = false;
+    for (let c of s) {
+        rows[currRow] += c;
+        if (currRow === 0 || currRow === numRows -1) goingDown = !goingDown;
+        currRow += goingDown ? 1 : -1;
+    }
+    return rows.join('');
+};
